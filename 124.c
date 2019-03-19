698a22f7ffeb10124a1edfab9b043a7456ab34dd
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-03-19 20:50:13 +0530

    124.c

---
+++ 124.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int n,c=1,a[10000],d,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    while(c)
+    {
+        d=0;
+        for(i=0;i<n;i++)
+        {
+            if(c%a[i]==0)
+            d++;
+        }
+        if(d==n)
+        break;
+        c++;
+    }
+    printf("%d",c);
+}
